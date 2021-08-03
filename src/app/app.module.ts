28978db4cd7bf4bf25db933ed3de27970f93381f
Emiliano Martinez <emartinezd16@gmail.com>
2021-08-03 00:31:06 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { ItemComponent } from './components/item/item.component';
+import { AsdasdComponent } from './components/asdasd/asdasd.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    ItemComponent,
+    AsdasdComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css'],
+})
+export class ItemComponent implements OnInit {
+  @Input() task!: Task;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
